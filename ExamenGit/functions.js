a848d1d85c62326055744509ec15dba7f3a7b4e3
Diego1887 <juandiegomartinezromo@gmail.com>
2024-06-06 09:15:30 -0600

    Editar una linea del JS en la rama FunctionBase

--- ExamenGit/functions.js
+++ ExamenGit/functions.js
@@ -5,6 +5,9 @@ Este ejemplo es ficticio y no representa a nada ni daie en particular
 No se fije en pequeñeses profe usted ponga un diez porfavor
 Andele prooooooooofeeeeeeeeeeeeeeeee
 
+
+Nueva linea de codigo aquí *Alumno procede a hacerse wey un rato*
+
 Te Kiere Don Ramon
 
 */
