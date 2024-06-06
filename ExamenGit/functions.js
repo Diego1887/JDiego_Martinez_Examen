5a3f47eaf664d8876484387357ebdc6ae22c6e5e
Diego1887 <juandiegomartinezromo@gmail.com>
2024-06-06 09:06:45 -0600

    Ageragr 5 lineas de codigo en el CSS y JS en la rama FunctionBase

---
+++ ExamenGit/functions.js
@@ -0,0 +1,10 @@
+/* 
+
+Este es un ejemplo de escritura dentro del Functions js
+Este ejemplo es ficticio y no representa a nada ni daie en particular
+No se fije en pequeñeses profe usted ponga un diez porfavor
+Andele prooooooooofeeeeeeeeeeeeeeeee
+
+Te Kiere Don Ramon
+
+*/
